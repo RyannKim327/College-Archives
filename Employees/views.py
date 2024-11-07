d7c0a0f7a839845a21cc15f977d7d61d9b0ab755
RyannKim327 <rksesgundo123@gmail.com>
2024-11-08 05:38:47 +0800

    11-08-24 05:38 [Added Update]

--- Employees/views.py
+++ Employees/views.py
@@ -5,6 +5,7 @@ from .models import Employee
 
 # Create your views here.
 
+# INFO: Read Data
 def index(request):
     emp = Employee.objects.all().values()
     template = loader.get_template("index.html")
@@ -12,3 +13,18 @@ def index(request):
         "employees": emp
     }
     return HttpResponse(template.render(context, request))
+
+# INFO: Update Data
+
+def edit(request,id):
+    emp = Employee.objects.get(emp_id=id)
+    template = loader.get_template("edit.html")
+    context = {
+        "employee": emp
+    }
+    return HttpResponse(template.render(context, request))
+
+def updateData(request):
+    if(request.method == "POST"):
+        data = request.POST
+        emp = Employee.objects.update()
