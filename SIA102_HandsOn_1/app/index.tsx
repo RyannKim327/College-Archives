1e8e51c3fb0f0c344ca32891c24f205fe2310c96
(13) (03) (-01) (01) <weryses19@gmail.com>
2025-09-11 16:42:41 +0800

    09-11-25 16:42 [Miss ko na sya prii]

--- SIA102_HandsOn_1/app/index.tsx
+++ SIA102_HandsOn_1/app/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Text, View } from "react-native";
 import Card from "./components/card";
 import Button from "./widgets/button";
 import Input from "./widgets/input";
+import axios from "axios"
 
 export default function Index() {
   const [l, setL] = useState(false);
@@ -10,9 +11,52 @@ export default function Index() {
   const [firstName, setFirstName] = useState("")
   const [lastName, setLastName] = useState("")
   const [section, setSection] = useState("")
-  const [status, setStatus] = useState("")
+  // const [status, setStatus] = useState("")
   const [message, setMessage] = useState("")
-  const [retruieveUsers, setRetruieveUsers] = useState([])
+  const [retrieveUsers, setRetrieveUsers] = useState([])
+
+  const submit = async (status: string) => {
+    setL(true)
+    try{
+      // const { data } = await axios.post(`https://sesgundo-class-manager-server.vercel.app/users/`,
+      //   {
+      //     firstName,
+      //     lastName,
+      //     section,
+      //     status
+      //   }
+      // )
+      const response = await fetch(`https://sesgundo-class-manager-server.vercel.app/users/`, {
+        "method": "POST",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        "body": JSON.stringify({
+          firstName,
+          lastName,
+          section,
+          status
+        })
+      })
+
+      console.log(response.ok)
+      if(!response.ok){
+        throw new Error("Server is not responding...")
+      }
+
+      const result = await response.json()
+
+      setMessage(result.message)
+    } catch(error){
+      console.error(error)
+      // setMessage(error.error)
+    }
+
+    setL(false)
+    setTimeout(() => {
+      setMessage("")
+    }, 2500)
+  }
 
   return (
     <View
@@ -28,9 +72,10 @@ export default function Index() {
           fontSize: 25,
           fontFamily: "serif"
         }}>SIA HandsOn #1</Text>
-        <Input label="First name" disabled={l} />
-        <Input label="Last name" disabled={l} />
-        <Input label="Section" disabled={l} />
+        <Input label="First name" onChange={setFirstName} disabled={l} />
+        <Input label="Last name" onChange={setLastName} disabled={l} />
+        <Input label="Section" onChange={setSection} disabled={l} />
+        <Text>{message}</Text>
         <View
           style={{
             display: "flex",
@@ -41,10 +86,10 @@ export default function Index() {
         >
           <Button
             onClick={() => {
-              setL(true);
-            }}
-            loading={l}
-          >
+                submit("Present")
+              }}
+              loading={l}
+              >
             Present
           </Button>
           <Button
@@ -52,7 +97,9 @@ export default function Index() {
               backgroundColor: "red",
             }}
             clickable={l}
-            onClick={() => {}}
+            onClick={() => {
+              submit("Absent")
+            }}
           >
             Absent
           </Button>
